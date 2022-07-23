99be41b913437b1511431d27dd2bba98c4888b61
FaisalMahamudCS <fmfahimcse@gmail.com>
2022-07-24 00:13:53 +0600

    facebook

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import auth from  '../firebase.init'
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from  './firebase.init'
+import { useSignInWithEmailAndPassword, useSignInWithGoogle,useSignInWithFacebook } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, fUser, fLoading, fError] = useSignInWithFacebook(auth);
+    const navigate = useNavigate();
+    if (gUser) {
+        navigate('/');
+    }
+
     return (
         <div>
+              <button onClick={() => SignInWithFacebook()}  className="btn btn-outline" >Continue with Google</button>
+ 
             <button onClick={() => signInWithGoogle()}  className="btn btn-outline" >Continue with Google</button>
  
         </div>
